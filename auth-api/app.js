0b334181798e57e20817bbb14dbbd19f304af787
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2022-08-29 21:54:51 -0300

    Configuração banco de dados

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import * as db from './src/config/db/initialData.js'
 const app = express()
 const env = process.env
-const PORT = env.PORT || 6000
+const PORT = env.PORT || 5000
 
 
+db.createInitialdata()
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: "Auth-API",
