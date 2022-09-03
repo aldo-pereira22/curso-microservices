caf80b5c4363d091f8088924f21aab3e8b99cae3
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2022-09-03 13:43:41 -0300

    Criando o service e o repository

---
+++ auth-api/src/modules/user/service/UserService.js
@@ -0,0 +1,40 @@
+import UserRepository from "../repository/UserRepository"
+import * as httpStatus from '../../../config/constants/httpStatus'
+
+
+class UserService {
+    async findByEmail(req) {
+        try {
+            const { email } = req.params
+            this.validateRequestData(email)
+            let user = UserRepository.findByEmail(email)
+            if (!user) {
+
+            }
+
+            return {
+                status: httpStatus.SUCCESS,
+                user: {
+                    id: user.id,
+                    nome: user.nome,
+                    email: user.email,
+
+
+                }
+            }
+        } catch (error) {
+            return {
+                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: error.message
+            }
+        }
+    }
+
+    validateRequestData(email) {
+        if (!email) {
+            throw new Error('User email was not informed.')
+        }
+    }
+}
+
+export default new UserService()
